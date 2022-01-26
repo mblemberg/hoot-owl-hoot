15bfa0d2be94180751eee5c60a0eaa7d9d15b28d
Mike Blemberg <mikeblemberg@gmail.com>
2022-01-26 00:35:51 -0600

    work on game class

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 
 CARDS_PER_HAND = 3
 DEFAULT_BOARD = 'ygobprbprygborpygobprgyobprygborpygborp'
+NEST_INDEX = 39
 
 SUN = 'S'
 YELLOW = 'y'
@@ -39,6 +40,15 @@ class Board:
         ret += str(self.sun_location)
 
         return ret
+
+    def next_empty_space(self, starting_index: int, card_color: str) -> int:
+        '''From the specified starting index, return the next empty space of the specified color.
+        If none exist, the nest is returned, index 39.'''
+        for index, space_color in enumerate(self.spaces[starting_index + 1:], starting_index + 1):
+            if space_color == card_color and index not in self.owl_locations:
+                return index
+        else:
+            return NEST_INDEX
 
 
 class Deck:
@@ -92,8 +102,96 @@ class Game:
         self.deck = Deck()
         self.deck.shuffle()
 
+        self.number_of_hands = number_of_hands
         self.hands = []
+        self.turn = 0
 
         for _ in range(number_of_hands):
             hand = self.deck.draw(number_of_cards=CARDS_PER_HAND)
             self.hands.append(hand)
+
+    def __repr__(self):
+        '''Returns a csv string representation of the game.'''
+
+        ret_val = self.board.__repr__() + ','
+        ret_val += self.deck.cards + ','
+        ret_val += ','.join(self.hands) + ','
+        ret_val += str(self.turn)
+
+        return ret_val
+
+    def move_owl(self, color: str, owl_index: int) -> None:
+        '''Moves the specified owl to the next empty space of the specified color.'''
+
+        owl_location = self.board.owl_locations[owl_index]
+        move_to_location = self.board.next_empty_space(owl_location, color)
+        self.board.owl_locations[owl_index] = move_to_location
+
+    def move_sun(self):
+        '''Moves the sun forward one position.'''
+
+        self.board.sun_location += 1
+
+    def state(self) -> str:
+        '''Returns "WON", "LOST", or "IN PROGRESS".'''
+
+        if self.board.sun_location == 13:
+            return 'LOST'
+        elif self.board.owl_locations.count(NEST_INDEX) == 6:
+            return 'WON'
+        else:
+            return 'IN PROGRESS'
+
+    def possible_moves(self) -> list:
+        '''Returns a list of possible moves.  Each move is represented as a list of two elements;
+        First, the color of the card as a single character.
+        Second, the index of the owl to be moved.  If a sun card is played,
+        the index of the owl can be ignored, but it will be set to 0.'''
+
+        moves = []
+
+        # If the hand contains a sun card, it must be played.
+        if SUN in self.hands[self.turn]:
+            moves.append([SUN, 0])
+            return moves
+
+        for card in self.hands[self.turn]:
+            for owl_index, owl_location in enumerate(self.board.owl_locations):
+                if owl_location != NEST_INDEX:
+                    moves.append([card, owl_index])
+
+        return moves
+
+    def move(self, move: list) -> None:
+        '''Makes a move.  Each move is represented as a list of two elements;
+        First, the color of the card as a single character.
+        Second, the index of the owl to be moved.  If a sun card is played,
+        the index of the owl can be ignored, but it will be set to 0.'''
+
+        if move[0] == 'S':
+            self.move_sun()
+        else:
+            self.move_owl(move[0], move[1])
+        
+        self.hands[self.turn] = self.hands[self.turn].replace(move[0], '', 1)
+        self.hands[self.turn] += self.deck.draw()
+
+        self.turn = (self.turn + 1) % self.number_of_hands
+
+
+g = Game()
+print(g)
+print(g.state())
+print(g.possible_moves())
+
+won = False
+while not won:
+    g = Game()
+    print(g)
+    while g.state() == 'IN PROGRESS':
+        possible_moves = g.possible_moves()
+        move_index = random.randint(0, len(possible_moves)-1)
+        g.move(g.possible_moves()[move_index])
+        print(g)
+    if g.state() == 'WON':
+        won = True
